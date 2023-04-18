17731b78a4113ae4919b26b238a2891bbffa40db
Barbara Soares <babisnunes14@gmail.com>
2023-04-18 09:04:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { wordsList } from './data/words';
+
+import { useEffect, useState, useCallback } from 'react';
+
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id:1, name: "start"},
+  { id:2, name: "game"},
+  { id:3, name: "end"},
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([""]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
+  console.log(words);
+
+  const pickWordAndCategory = useCallback(() => {
+    const categories = Object.keys(words);
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    const word = 
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(category, word);
+
+    return { category, word };
+  }, [words]);
+
+ const startGame = useCallback(() => {
+  
+  clearLettersStates();
+
+  const { category, word } = pickWordAndCategory();
+
+  console.log(category, word);
+
+  let wordLetters = word.split("");
+
+  wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+  console.log(wordLetters);
+
+  setPickedWord(word);
+  setPickedCategory(category);
+  setLetters(wordLetters);
+
+  setGameStage(stages[1].name);
+ }, [pickWordAndCategory]);
+
+ const verifyLetter = (letter) => {
+  const normalizedLetter = letter.toLowerCase();
+
+  if (
+    guessedLetters.includes(normalizedLetter) ||
+    wrongLetters.includes(normalizedLetter)
+  ) {
+    return;
+  }
+
+  if (letters.includes(normalizedLetter)) {
+    setGuessedLetters((actualGuessedLetters) => [
+      ...actualGuessedLetters,
+      letter,
+    ]);
+  } else {
+    setWrongLetters((actualWrongLetters) => [
+      ...actualWrongLetters,
+      normalizedLetter,
+    ]);
+
+    setGuesses((actualGuesses) => actualGuesses - 1);
+  }
+};
+
+console.log(wrongLetters);
+
+ const retry = () => {
+  setScore(0);
+  setGuesses(3);
+  setGameStage(stages[0].name);
+ }
+
+ const clearLettersStates = () => {
+  setGuessedLetters([]);
+  setWrongLetters([]);
+};
+
+ useEffect(() => {
+  if (guesses === 0) {
+    clearLettersStates();
+
+    setGameStage(stages[2].name);
+  }
+}, [guesses]);
+
+useEffect(() => {
+  const uniqueLetters = [...new Set(letters)];
+
+  console.log(uniqueLetters);
+  console.log(guessedLetters);
+
+  if (guessedLetters.length === uniqueLetters.length) {
+    setScore((actualScore) => (actualScore += 100));
+
+    startGame();
+  }
+}, [guessedLetters, letters, startGame]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameStage === 'start' &&  <StartScreen startGame={startGame}/>}
+      {gameStage === 'game' &&  (
+        <Game 
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === 'end' &&  <GameOver retry={retry} score={score}/>}
     </div>
   );
 }
---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cabeça", "Pescoço", "Olhos"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    animal: ["Leão", "Cachorro", "Gato", "Elefante", "Cobra"],
+    objeto: ["Cadeira", "Sofá", "Computador", "Mesa", "Prato"],
+  };
